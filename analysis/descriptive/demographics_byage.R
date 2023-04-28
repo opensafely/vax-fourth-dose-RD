2825c6baa4cbc3a899c46c68576808e669ee996f
Andrea Schaffer <37759997+alschaffer@users.noreply.github.com>
2023-04-28 14:35:38 +0100

    Fixed age / death issue

--- analysis/descriptive/demographics_byage.R
+++ analysis/descriptive/demographics_byage.R
@@ -107,16 +107,19 @@ demographics_by_age <- rbind(imd, sex, region, ethnicity)
 #############################################################
 
 fluvax <- read_csv(here::here("output", "cohort", "cohort_final_sep.csv")) %>%
-  dplyr::select(c(age, dob, dod, flu_vax_date)) %>%
-  subset(age >= 45 & age < 55 &
-           (is.na(dod) | dod >= as.Date("2022-09-03"))) %>%
+  dplyr::select(c(dob, dod, flu_vax_date)) %>%
   
   # Create age in months variable
-  mutate(age_mos = (dob %--% "2022-11-26") %/% months(1),
+  mutate(age = (dob %--% "2022-11-26") %/% years(1),
+         age_mos = (dob %--% "2022-11-26") %/% months(1),
          age_3mos = floor(age_mos / 3),
-         flu_vax = if_else(!is.na(flu_vax_date) & flu_vax_date < "2022-11-26",
-                          1, 0, 0)) %>%
+         flu_vax = if_else(!is.na(flu_vax_date) & 
+                             flu_vax_date < "2022-11-26",
+                           1, 0, 0)) %>%
   
+  subset(age >= 45 & age < 55 &
+           (is.na(dod) | dod >= as.Date("2022-11-26"))) %>%
+         
   # Calculate denominator by age in months
   group_by(age_3mos) %>%
   mutate(total_age_3mos = n()) 
