6dbafce312da34cf81d04f8acad973514d6766a5
Andrea Schaffer <37759997+alschaffer@users.noreply.github.com>
2023-08-15 14:53:25 +0100

    Aggregate outcomes

--- analysis/processing/aggregate_outcomes_entire_period.R
+++ analysis/processing/aggregate_outcomes_entire_period.R
@@ -2,7 +2,6 @@
 ################################################################
 # This script:
 # - Calculates number of outcomes by age for tables and figures
-#
 ################################################################
 
 
@@ -37,69 +36,68 @@ source(here::here("analysis", "custom_functions.R"))
 #########################################
 
 
-# No redaction
-df1 <- read_feather(here::here("output", "input_outcomes_2022-11-26.feather")) %>%
+# Sep 3
+df1 <- read_feather(here::here("output", "input_outcomes_2022-09-03.feather")) %>%
   mutate(dob = as.Date(as.character(as.POSIXct(dob)), format = "%Y-%m-%d"),
          
          # Set DOB to mid-month
          dob = dob + 14,
          
          # Calculate age at index date
-         age_yrs = (dob %--% as.Date("2022-11-26")) %/% years(1),
-         age_mos = (dob %--% as.Date("2022-11-26")) %/% months(1),
+         age_yrs = (dob %--% as.Date("2022-09-03")) %/% years(1),
+         age_mos = (dob %--% as.Date("2022-09-03")) %/% months(1),
          age_3mos = floor(age_mos / 3),
          over50 = if_else(age_yrs >= 50 & age_yrs < 55, 1, 0, 0),
-         nov26 = 1) %>%
+         sep3 = 1) %>%
+  
+  # Drop if outside age range or died before index date
+  subset(age_yrs >= 45 & age_yrs < 55 & 
+           (is.na(dod) | dod >= as.Date("2022-09-03"))) %>%
+  dplyr::select(c("patient_id", "covidcomposite", "respcomposite", "anydeath", 
+                  "anyadmitted", "over50", "sep3"))
+
+# Oct 15
+df2 <- read_feather(here::here("output", "input_outcomes_2022-10-15.feather")) %>%
+  mutate(dob = as.Date(as.character(as.POSIXct(dob)), format = "%Y-%m-%d"),
+         
+         # Set DOB to mid-month
+         dob = dob + 14,
+         
+         # Calculate age at index date
+         age_yrs = (dob %--% as.Date("2022-09-03")) %/% years(1),
+         age_mos = (dob %--% as.Date("2022-09-03")) %/% months(1),
+         age_3mos = floor(age_mos / 3),
+         over50 = if_else(age_yrs >= 50 & age_yrs < 55, 1, 0, 0),
+         sep3 = 1) %>%
+  
+  # Drop if outside age range or died before index date
+  subset(age_yrs >= 45 & age_yrs < 55 & 
+           (is.na(dod) | dod >= as.Date("2022-10-15"))) %>%
+  dplyr::select(c("patient_id", "covidcomposite", "respcomposite", "anydeath", 
+                  "anyadmitted", "over50", "sep3"))
+
+# Nov 26
+df3 <- read_feather(here::here("output", "input_outcomes_2022-11-26.feather")) %>%
+  mutate(dob = as.Date(as.character(as.POSIXct(dob)), format = "%Y-%m-%d"),
+         
+         # Set DOB to mid-month
+         dob = dob + 14,
+         
+         # Calculate age at index date
+         age_yrs = (dob %--% as.Date("2022-09-03")) %/% years(1),
+         age_mos = (dob %--% as.Date("2022-09-03")) %/% months(1),
+         age_3mos = floor(age_mos / 3),
+         over50 = if_else(age_yrs >= 50 & age_yrs < 55, 1, 0, 0),
+         sep3 = 1) %>%
   
   # Drop if outside age range or died before index date
   subset(age_yrs >= 45 & age_yrs < 55 & 
            (is.na(dod) | dod >= as.Date("2022-11-26"))) %>%
   dplyr::select(c("patient_id", "covidcomposite", "respcomposite", "anydeath", 
-                  "anyadmitted", "over50", "nov26"))
+                  "anyadmitted", "over50", "sep3"))
   
-  
-# No redaction
-read <- function(start_date){
-  
-  # No redaction
-  dat <- read_feather(here::here("output", paste0("input_outcomes_",start_date,".feather"))) %>%
-    mutate(dob = as.Date(as.character(as.POSIXct(dob)), format = "%Y-%m-%d"),
 
-           # Set DOB to mid-month
-           dob = dob + 14,
-           
-           # Calculate age at index date
-           age_yrs = (dob %--% as.Date(start_date)) %/% years(1),
-           age_mos = (dob %--% as.Date(start_date)) %/% months(1),
-           age_3mos = floor(age_mos / 3),
-           over50 = if_else(age_yrs >= 50 & age_yrs < 55, 1, 0, 0),
-           nov26 = 0) %>%
-    
-    # Drop if outside age range or died before index date
-    subset(age_yrs >= 45 & age_yrs < 55 & 
-             (is.na(dod) | dod >= as.Date(start_date))) %>%
-    dplyr::select(c("patient_id", "covidcomposite", "respcomposite", "anydeath", 
-                    "anyadmitted", "over50", "nov26"))
-  
-  return(dat)
-  
-}
-
-df2 <- read("2022-11-27")
-df3 <- read("2022-11-28")
-df4 <- read("2022-11-29")
-df5 <- read("2022-11-30")
-df6 <- read("2022-12-01")
-df7 <- read("2022-12-02")
-df8 <- read("2022-12-03")
-df9 <- read("2022-12-04")
-df10 <- read("2022-12-05")
-df11 <- read("2022-12-06")
-df12 <- read("2022-12-07")
-df13 <- read("2022-12-08")
-df14 <- read("2022-12-09")
-  
-all <- rbind(df1,df2,df3,df4,df5,df6,df7,df8,df9,df10,df11,df12,df13,df14) %>%
+all <- rbind(df1,df2,df3) %>%
   mutate(covidcomposite = ifelse(covidcomposite == TRUE, 1, 0),
          respcomposite = ifelse(respcomposite == TRUE, 1, 0),
          anydeath = ifelse(anydeath == TRUE, 1, 0),
@@ -109,8 +107,8 @@ all <- rbind(df1,df2,df3,df4,df5,df6,df7,df8,df9,df10,df11,df12,df13,df14) %>%
             respcomposite = max(respcomposite),
             anydeath = max(anydeath),
             anyadmitted = max(anyadmitted),
-            nov26 = max(nov26)) %>%
-  subset(nov26 == 1)
+            sep3 = max(sep3)) %>%
+  subset(sep3 == 1)
 
 
 total <- all %>%
